66d7b7174d980e9d3869611ffe9a5ea5d56486a7
AnCoelho <arthurnunes6@hotmail.com>
2024-10-24 20:15:39 -0300

    first

---
+++ src/test/java/com/projeto/contabix/ContabixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.contabix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
