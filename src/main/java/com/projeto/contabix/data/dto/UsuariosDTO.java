30f7f18cd3104641931d341a86d9892d5cebdadf
AnCoelho <arthurnunes6@hotmail.com>
2024-10-31 21:19:08 -0300

    Alterações

--- src/main/java/com/projeto/contabix/data/dto/UsuariosDTO.java
+++ src/main/java/com/projeto/contabix/data/dto/UsuariosDTO.java
@@ -1,23 +1,22 @@
-package br.poo.unipac.uberlandia.ematriculasserver.data.dto;
+package com.projeto.contabix.data.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UsuariosDTO {
-    private Long oidUsuarios;
+public class UsuariosDTO extends TipoUsuarioDTO {
+    private Long idUsuario;
     private String nome;
-    private String nrCpf;
-    private Date dtNascimento;
-    private String nmResponsavel;
-    private Double notaVestibular;
-    private String dsStatus;
-    private String dsSenha;
-    private Long oidPeriodo;
-    private Double vrBolsa;
+    private String usuario;
+    private String cnpj;
+    private String email;
+    private String senha;
+    private Long idTipoUsuario; 
+    private boolean ativo;
+    private LocalDateTime dataCriacao;
 }
