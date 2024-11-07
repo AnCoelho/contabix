0f93c075ceecb9da92ec25821351232f483e25c4
Eduardo <ti22mau@gmail.com>
2024-11-07 01:32:52 -0300

    fix(agenda-entity): corrigindo relação entre tabelas

--- src/main/java/com/projeto/contabix/data/entity/AgendaEntity.java
+++ src/main/java/com/projeto/contabix/data/entity/AgendaEntity.java
@@ -8,12 +8,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "AGENDA")
 public class AgendaEntity {
     @Id
@@ -21,7 +25,7 @@ public class AgendaEntity {
     @Column(name = "ID_EVENTO")
     private Long idEvento;
 
-    @ManyToMany
+    @ManyToOne
     @JoinColumn(name = "ID_USUARIO")
     private UsuariosEntity usuario;
 
